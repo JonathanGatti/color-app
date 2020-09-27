cc36ceb36b895a1e278a13e93a179c7b9adb30e8
jonathan gatti <jonathangatti2@gmail.com>
2020-09-27 11:14:56 +0100

    saved palettes to local storage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import './App.css';
 class  App extends Component  {  
   constructor(props){
     super(props);
-    this.state = { palettes: seedColors}
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+    this.state = { palettes: savedPalettes || seedColors}
   }
   findPalette = (id) => {
     return this.state.palettes.find((palette) => {
@@ -19,7 +20,13 @@ class  App extends Component  {
     })
   }
   savePalette = (newPalette) =>{
-    this.setState({palettes: [...this.state.palettes, newPalette]})
+    this.setState({palettes: [...this.state.palettes, newPalette]}, 
+      this.syncLocalStorage
+      );
+  }
+
+  syncLocalStorage = () =>{
+    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
   }
 
   render(){
