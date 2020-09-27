0c787b429039b1fd6ab44feeee4dfc47439f984e
jonathan gatti <jonathangatti2@gmail.com>
2020-09-27 10:43:32 +0100

    moved out styles

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,54 +1,18 @@
-import React from 'react';
-import { SortableElement } from 'react-sortable-hoc';
-import { withStyles } from '@material-ui/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React from "react";
+import { SortableElement } from "react-sortable-hoc";
+import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+import styles from "./styles/DraggableColorBoxStyles";
 
-
-const styles = {
-  root: {
-    width: "20%",
-    height: '25%',
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    '&:hover svg': {
-      color: 'white',
-      transform: 'scale(1.2)'
-    }
-  },
-  boxContent: {
-    position: "absolute",
-    width: "100%",
-    left: "0px",
-    bottom: "0px",
-    padding: "10px",
-    color: "black",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "12px",
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-  deleteIcon: {
-    color: 'rgba(0,0,0,0.5)',
-    transition: 'all 0.3s ease-in-out'
-  }
-}
-
-const DraggableColorBox = SortableElement((props) =>{
-  const { classes, color, name, handleClick } = props;
-  return(
-    <div 
-      className={classes.root} 
-      style={{backgroundColor: color}}>
-      <div className={classes.boxContent}>  
-        <span>{name}</span>
-        <DeleteIcon className={classes.deleteIcon} onClick={handleClick}/>
-      </div>  
+const DraggableColorBox = SortableElement(props => {
+  const { classes, handleClick, name, color } = props;
+  return (
+    <div className={classes.root} style={{ backgroundColor: color }}>
+      <div className={classes.boxContent}>
+        <span> {name}</span>
+        <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
+      </div>
     </div>
-  )
-})
-
+  );
+});
 export default withStyles(styles)(DraggableColorBox);
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import PaletteMetaForm from './PaletteMetaForm';
+import PaletteMetaForm from "./PaletteMetaForm";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -10,64 +10,31 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-
-const drawerWidth = 400;
-const styles = theme => ({
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: 'center',
-    height: "64px",
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20
-  },
-  navBtns: {
-    marginRight: "1rem",
-    "& a": {
-       textDecoration: "none"
-     }    
- },
-  button: {
-    margin: '0 0.5rem',
-    textDecoration:'none'
-  }
-});
+import styles from "./styles/PaletteFormNavStyles";
 
 class PaletteFormNav extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      newPaletteName: "",
-      formShowing: false
-    };
+    this.state = { newPaletteName: "", formShowing: false };
+    this.handleChange = this.handleChange.bind(this);
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
   }
 
-  showForm = () => {
-    this.setState({formShowing: true})
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
   }
-
-  hideForm = () => {
-    this.setState({formShowing: false})
+  showForm() {
+    this.setState({ formShowing: true });
+  }
+  hideForm() {
+    this.setState({ formShowing: false });
   }
   render() {
     const { classes, open, palettes, handleSubmit } = this.props;
+    const { newPaletteName } = this.state;
     return (
       <div className={classes.root}>
         <CssBaseline />
@@ -93,28 +60,30 @@ class PaletteFormNav extends Component {
           </Toolbar>
           <div className={classes.navBtns}>
             <Link to='/'>
-              <Button 
-                variant='contained' 
-                className={classes.button} 
-                color='secondary'>
+              <Button
+                variant='contained'
+                color='secondary'
+                className={classes.button}
+              >
                 Go Back
               </Button>
             </Link>
-            <Button 
-              variant="contained" 
-              className={classes.button} 
-              color="primary" 
-              onClick={this.showForm}>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={this.showForm}
+              className={classes.button}
+            >
               Save
             </Button>
           </div>
         </AppBar>
         {this.state.formShowing && (
-          <PaletteMetaForm 
-            palettes={palettes} 
-            handleSubmit={handleSubmit} 
+          <PaletteMetaForm
+            palettes={palettes}
+            handleSubmit={handleSubmit}
             hideForm={this.hideForm}
-            />
+          />
         )}
       </div>
     );
