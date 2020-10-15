2ab56a686790599aeb2689376942abb83e525532
jonathan gatti <jonathangatti2@gmail.com>
2020-10-15 18:12:17 +0100

    Updates

---
+++ precache-manifest.fa076d78e5ae997a7f01edf1bf8ff683.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6eed0a870f4892065f88a5db9d0824",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "7d5395011867b48e2069",
+    "url": "/color-app/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "ab5829ab858b92b43e71",
+    "url": "/color-app/static/css/main.3dca02f1.chunk.css"
+  },
+  {
+    "revision": "7d5395011867b48e2069",
+    "url": "/color-app/static/js/2.53fe3b51.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-app/static/js/2.53fe3b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5829ab858b92b43e71",
+    "url": "/color-app/static/js/main.a4a0b6e1.chunk.js"
+  },
+  {
+    "revision": "6ef70906bf917036e363",
+    "url": "/color-app/static/js/runtime-main.74a067ae.js"
+  },
+  {
+    "revision": "1d95f461ced1ab8f667aa6e65a5bbe35",
+    "url": "/color-app/static/media/bg.1d95f461.svg"
+  }
+]);
